f6a1dc7955cbef3e0710b6a0496152a2b9c261c4
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-02-07 20:51:27 +0900

    fix detail

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 //ここではアプリケーション内のReducerを全て総括するファイルです
 
 import { combineReducers } from 'redux';
-import count from './count'
+import count from './counter.js';
 
 export default combineReducers({ count })
