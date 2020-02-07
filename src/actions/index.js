dbbbc46dd0b474961ab04235d8f2519ca035ee8d
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-02-07 19:31:28 +0900

    learn redux-action

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+//actionCreaterを生成
+
+export const INCREMENT = "INCREMENT" 
+export const DECREMENT = "DECREMENT" 
+
+export const increment = () => ({
+        type: INCREMENT
+})
+export const decrement = () => ({
+        type: DECREMENT
+})
+
+
