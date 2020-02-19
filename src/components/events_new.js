c35424303c20beb50eb2d4a5ecb54f436061365d
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-02-19 20:46:45 +0900

    error

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -2,19 +2,42 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom';
 //import { postEvent } from '../actions';
+import { Field, reduxForm } from 'redux-form';
 
 
 class EventsNew extends Component {
+renderField(field){
+  const { input, type, meta: { touched, error } } = field
+
+  return (<div></div>)
+}
+
   render() {
     return (
-      <React.Fragment>
-        <div>foo</div>
-      </React.Fragment>
+      <form>
+          <div>
+            <Field label="Title" name="title" type="text" Component={this.renderField} />
+          </div>
+          <div>
+            <Field label="Body" name="body" type="text" Component={this.renderField} />
+          </div>
+          <div>
+            <input type="submit" value="Submit" disabled={false}/>
+            <Link to="/">Cancel</Link>
+          </div>
+      </form>
     )
   }
 }
 
+
+const validate = values => {
+  const errors = {}
+  return errors
+}
 // const mapDispatchToProps = ({postEvent})
 
 //stateとactionをコンポーネントに関連付ける実装
-export default connect(null, null)(EventsNew)
+export default connect(null, null)(
+  reduxForm({ validate, form: 'eventNewForm'})(EventsNew)
+  )
