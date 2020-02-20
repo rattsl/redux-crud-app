d90f54034c533e40a8f0b188a27255b60a4b0fc4
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-02-20 22:11:20 +0900

    implement delete feature

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import thunk from 'redux-thunk';
 //componentsを全てインポート
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
+import EventsShow from './components/events_show';
 
 //Storeを作成するためのメソッド
 import { createStore, applyMiddleware } from 'redux';
@@ -22,16 +23,23 @@ import reducer from './reducers'
 //routerでラップする
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+//redux-devtoolを使う
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 //Store作成
-const store = createStore(reducer, applyMiddleware(thunk))
+const enhancer = process.env.NODE_ENV === "development" ?
+composeWithDevTools(applyMiddleware(thunk)): applyMiddleware(thunk)
+
+const store = createStore(reducer, enhancer)
 
 //次にここで作成したストアがどのコンポーネントからも参照できるようにProviderを使う。そしてstore属性に作成したstoreを渡す。
 ReactDOM.render(
 <Provider store={store}>
     <BrowserRouter>
         <Switch>
-            <Route exact path="/events/new" component={EventsNew} />
+            <Route path="/events/new" component={EventsNew} />
+            <Route path="/events/:id" component={EventsShow} />
+            <Route exact path="/events" component={EventsIndex} />
             <Route exact path="/" component={EventsIndex} />
         </Switch>
     </BrowserRouter>
